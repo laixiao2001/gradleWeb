aa67663d21afe029b62e573022015f6323532a36
刘晓文 <liuxiaowen@thinkjoy.cn>
2016-10-25 14:13:39 +0800

    add config

---
+++ src/main/java/com/test/jpa/User.java
@@ -0,0 +1,8 @@
+package com.test.jpa;
+
+/**
+ * Created by liusven on 2016/10/25.
+ */
+public class User
+{
+}
---
+++ src/main/java/com/test/jpa/user/IUserDao.java
@@ -0,0 +1,8 @@
+package com.test.jpa.user;
+
+/**
+ * Created by liusven on 2016/10/25.
+ */
+public class IUserDao
+{
+}
